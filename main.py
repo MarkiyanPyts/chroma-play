9673f8f153b208d917a58ee6ecf53e10709c5c76
Markiyan Pyts <markiyan.pyts@gmail.com>
2025-08-20 11:18:03 +0300

    start

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from chroma-play!")
+import chromadb
+chroma_client = chromadb.Client()
 
+collection_name = "test_collection"
+collection = chroma_client.get_or_create_collection(name=collection_name)
 
-if __name__ == "__main__":
-    main()
+documents = [
+    {"id": "doc1", "text": "Hello, world!"},
+    {"id": "doc2", "text": "How are you today?"},
+    {"id": "doc3", "text": "Goodbye, see you later!"},
+    {
+        "id": "doc4",
+        "text": "Microsoft is a technology company that develops software. It was founded by Bill Gates and Paul Allen in 1975.",
+    },
+]
+
+for doc in documents:
+    collection.upsert(ids=[doc["id"]], documents=[doc["text"]])
+
+query = "Hello, world !"
+
+results = collection.query(query_texts=[query], n_results=2)
+
+print(results)
